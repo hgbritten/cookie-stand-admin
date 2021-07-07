47788626cbaeec7fd47f3641be526b23a6a98659
hunter britten <huntergbritten@gmail.com>
2021-07-06 18:14:09 -0700

    link in?

--- pages/index.js
+++ pages/index.js
@@ -2,81 +2,71 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>
+          Cookie Stand Admin
+        </title>
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className="bg-green-500 px-5 py-3">
+      <h1 className="text-black text-3xl">
+        Cookie Stand Admin
+      </h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <main className="h-96 mx-auto">
+      <div className="h-88 mx-auto">
+
+        <form className="justify-center h-168 bg-green-300 mx-auto p-2 mx-10 my-5 rounded">
+          <div>
+            <h2 className="min-w-full text-center mx-auto p-2">
+              Create Cookie Stand
+            </h2>
+            <div className="flex m-2">
+
+              <label className="mx-2">Location</label>
+              <input type="text" className="flex-auto" />
+            </div>
+            <div className="flex py-10 mx-1 items-center">
+              <div className="flex">
+                <div className="justify-items-start">
+                  <label className="py-10">Minimum Customers per Hour</label>
+                  <input type="text" className="mx-1" />
+                </div>
+                <div>
+                  <label>Maximum Customers per Hour</label>
+                  <input type="text" className="mx-1" />
+                </div>
+                <div>
+                  <label>Average Cookies per Sale</label>
+                  <input type="text" className="mx-1" />
+                </div>
+              </div>
+              <button type="submit" className="w-48 h-20 bg-green-500">Create</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  )
+}
+function Footer() {
+  return (
+    <footer>
+      <p className="bg-green-500 px-5 py-3">&copy; 2021</p>
+    </footer>
   )
 }
